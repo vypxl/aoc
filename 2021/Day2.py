9a4a46198217088d8fbcb7b51b09c199b7eff72f
vypxl <thomas@vypxl.io>
2021-12-02 07:59:00 +0100

    Day2: hacky python

---
+++ 2021/Day2.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# pylint: disable=unused-wildcard-import
+from util import *
+
+def parse(inp):
+    return lmap(lambda l: l.split(' '), inp.splitlines())
+
+def p1(inp):
+    d = 0
+    p = 0
+    for xs in inp:
+        cmd, x = xs
+        x = int(x)
+        
+        if cmd == 'forward':
+            p += x
+        if cmd == 'down':
+            d += x
+        if cmd == 'up':
+            d -= x
+    return d * p
+
+def p2(inp):
+    d = 0
+    p = 0
+    a = 0
+    for xs in inp:
+        cmd, x = xs
+        x = int(x)
+        
+        if cmd == 'forward':
+            p += x
+            d += a*x
+        if cmd == 'down':
+            a += x
+        if cmd == 'up':
+            a -= x
+    return d * p
+
+def main():
+    inp = parse(data())
+    print(f"Solution for part 1:\n{p1(inp)}")
+    print(f"Solution for part 2:\n{p2(inp)}")
+
+if __name__ == "__main__":
+    main()
+
+# Solution part 1: 1804520
+# Solution part 2: 1971095320
