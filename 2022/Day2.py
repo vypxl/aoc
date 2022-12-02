261fa6f9ddd14b0f9e30d51c7ef58fdc80178254
vypxl <thomas@vypxl.io>
2022-12-02 06:43:44 +0100

    Day 2 clean

--- 2022/Day2.py
+++ 2022/Day2.py
@@ -3,50 +3,13 @@
 from util import *
 
 def parse(inp):
-    return [x.split() for x in inp.splitlines()]
-
-xs = {
-    'A': 1,
-    'B': 2,
-    'C': 3,
-    'X': 1,
-    'Y': 2,
-    'Z': 3,
-}
-
-def score(a, b):
-    if a == 'A' and b == 'Y' or a == 'B' and b == 'Z' or a == 'C' and b == 'X': return 6
-    if a == 'A' and b == 'X' or a == 'B' and b == 'Y' or a == 'C' and b == 'Z': return 3
-    return 0
-
-# def draw(a, b):
-
-def win(a):
-    if a == 'A': return 'Y'
-    if a == 'B': return 'Z'
-    if a == 'C': return 'X'
-
-def draw(a):
-    if a == 'A': return 'X'
-    if a == 'B': return 'Y'
-    if a == 'C': return 'Z'
-
-def lose(a):
-    if a == 'A': return 'Z'
-    if a == 'B': return 'X'
-    if a == 'C': return 'Y'
-
-which = {
-    'Z': win,
-    'Y': draw,
-    'X': lose,
-}
+    return [(ord(c) - ord('A'), ord(d) - ord('X')) for c, d in [x.split() for x in inp.splitlines()]]
 
 def p1(inp):
-    return sum(xs[b] + score(a, b) for a, b in inp)
+    return sum(1 + b + ((b - a + 1) % 3) * 3 for a, b in inp)
 
 def p2(inp):
-    return sum(xs[which[b](a)] + score(a, which[b](a)) for a, b in inp)
+    return p1((a, (a + b - 1) % 3) for a, b in inp)
 
 def main():
     inp = parse(data())
