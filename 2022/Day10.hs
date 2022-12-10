36bea4ecead1137566b01202592d17f56a2d24f9
vypxl <thomas@vypxl.io>
2022-12-10 19:21:50 +0100

    Day 10 haskell faster

--- 2022/Day10.hs
+++ 2022/Day10.hs
@@ -1,38 +1,39 @@
 #! /usr/bin/env -S runhaskell
 import Data.Ix (inRange)
-import Data.List (intercalate)
-import Data.List.Split (chunksOf, splitOn)
 import Data.Char (ord)
+
+import qualified Data.ByteString.Lazy.Char8 as B
+import Prelude hiding (readFile)
 
 import Criterion.Main
 
 import Util
 
 data Instr = Noop | Addx Int deriving (Show)
-data CPU = CPU { cycl :: Int, x :: Int, strength :: Int, screen :: String }
-
-readInstr :: String -> Instr
-readInstr s = case drop 5 s of [] -> Noop; r -> Addx (readI r)
+data CPU = CPU { cycl :: Int, x :: Int, offset :: Int, strength :: Int, screen :: String }
 
 readC :: Char -> Int
 readC c = ord c - ord '0'
 
-readI :: String -> Int
-readI ('-':xs) = -(readI xs)
-readI [x, y] = (readC x) * 10 + (readC y)
-readI [c] = readC c
-
 parse :: String -> [Instr]
-parse = map readInstr . splitOn "\n"
+parse = fst . go
+  where
+    go ('n':_:_:_:_:xs) = let (is, rest) = go xs in (Noop:is, rest)
+    go (_:_:_:_:_:c:'\n':xs) = let (is, rest) = go xs in ((Addx $ readC c):is, xs)
+    go (_:_:_:_:_:'-':c:'\n':xs) = let (is, rest) = go xs in ((Addx $ - readC c):is, xs)
+    go (_:_:_:_:_:'-':x:y:_:xs) = let (is, rest) = go xs in ((Addx $ -((readC x) * 10 + (readC y))):is, rest)
+    go (_:_:_:_:_:x:y:_:xs) = let (is, rest) = go xs in ((Addx $ (readC x) * 10 + (readC y)):is, rest)
+    go [] = ([], [])
 
 solve :: [Instr] -> (Int, String)
-solve xs = let CPU { strength, screen } = foldl (flip doInstr) (CPU 1 1 0 "") xs in (strength, intercalate "\n" . chunksOf 40 $ reverse screen)
+solve xs = let CPU { strength, screen } = foldl (flip doInstr) (CPU 1 1 1 0 "") xs in (strength, reverse screen)
   where
-    doCycle delta st@(CPU cycl x strength screen) = st {
+    doCycle delta st@(CPU cycl x offset strength screen) = st {
       cycl = cycl + 1,
+      offset = 40 `divides` cycl ? 1 $ offset + 1,
       x = x + delta,
-      strength = (40 `divides` (cycl + 20) ? (cycl * x) $ 0) + strength,
-      screen = (inRange (x, x + 2) ((cycl - 1) `mod` 40 + 1) ? '█' $ ' ') : screen
+      strength = (cycl * x * fromEnum (40 `divides` (cycl + 20))) + strength,
+      screen = let c = inRange (x, x+2) offset ? '█' $ ' ' in offset == 40 ? '\n':c:screen $ c:screen
     }
     doInstr Noop = doCycle 0
     doInstr (Addx delta) = doCycle delta . doCycle 0
@@ -40,22 +41,20 @@ solve xs = let CPU { strength, screen } = foldl (flip doInstr) (CPU 1 1 0 "") xs
 main :: IO ()
 main = do
   let fname = "10.in"
-  f <- readFile fname
-  let input = parse f
+  f <- B.readFile fname
+  let input = parse $ B.unpack f
+  main'
   defaultMain [
-    bgroup "Day 10" [ bench "Full" $ nfIO $ (\f -> readFile f >>= (return . solve . parse)) fname
-                    , bench "Parse + Solve, no IO" $ nf (solve . parse) f
+    bgroup "Day 10" [ bench "Full" $ nfIO $ (\f -> B.readFile f >>= (return . solve . parse . B.unpack)) fname
+                    , bench "Parse + Solve, no IO" $ nf (solve . parse . B.unpack) f
                     , bench "Solve only" $ nf solve input
-                    , bench "read -42" $ nf (readI :: (String -> Int)) "-42"
                     ]
     ]
-  main'
 
 main' :: IO()
 main' = do
-    f <- readFile("10.in")
-    let input = parse f
-    print input
+    f <- B.readFile "10.in"
+    let input = parse $ B.unpack f
     let (p1, p2) = solve input
     putStr "Solution for part 1: "
     putStrLn . show $ p1
