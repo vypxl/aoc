663ca907f2bde5835311826c6f59c06f0e809248
vypxl <thomas@vypxl.io>
2024-12-05 21:05:21 +0100

    fix rust template

--- 2024/template.rs
+++ 2024/template.rs
@@ -3,16 +3,16 @@ use std::io::{self, BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let inp = BufReader::new(io::stdin());
-    let p1 = 0;
-    let p2 = 0;
+    let mut p1 = 0;
+    let mut p2 = 0;
 
     for line in inp.lines() {
         let line = line?;
         let ints: Vec<i32> = line.split(" ").map(|s| s.parse::<i32>().unwrap()).collect();
     }
 
-    println!("Part-1: {}", p1);
-    // println!("Part-2: {}", p2);
+    println!("Part1: {}", p1);
+    // println!("Part2: {}", p2);
 
     Ok(())
 }
